90ee4c25b1cc1ef945f890f78774a2c922e001e5
Matt Guttman <msguttman@gmail.com>
2020-04-22 03:01:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,20 @@
 def new_hash
-  
-  # return an empty hash
+  new_hash = hash.new
+    # return an empty hash
 end
 
 def my_hash
+  my_hash = {1: "two", 2: "three"}
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  new_hash = {name: "Grace Hopper"}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  new_hash = {id: "number"}
   # return a hash with a key :id assigned to the provided number
 end
